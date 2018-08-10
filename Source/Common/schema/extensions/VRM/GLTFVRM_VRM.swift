fa8d746d8ad331c231a02d843d0ac6c7fb93c62f
magicien <magicien.du.ballon@gmail.com>
2018-08-11 05:21:10 +0900

    Remove public from VRM, set metadata to SCNScene

--- Source/Common/schema/extensions/VRM/GLTFVRM_VRM.swift
+++ Source/Common/schema/extensions/VRM/GLTFVRM_VRM.swift
@@ -5,145 +5,163 @@
 import Foundation
 import SceneKit
 
-public struct GLTFVRM_GLTFVRMExtension: GLTFCodable {
-    public struct GLTFVRM_VRM: Codable {
-        public let exportVersion: String?
-        public let meta: GLTFVRM_GLTFVRMMeta
-        public let humanoid: GLTFVRM_GLTFVRMHumanoid
-        public let firstPerson: GLTFVRM_GLTFVRMFirstperson
-        public let blendShapeMaster: GLTFVRM_GLTFVRMBlendShapeMaster
-        public let secondaryAnimation: GLTFVRM_GLTFVRMSecondaryAnimation
-        public let materialProperties: [GLTFVRM_GLTFVRMMaterialProperties]
+struct GLTFVRM_GLTFVRMExtension: GLTFCodable {
+    struct GLTFVRM_VRM: Codable {
+        let exportVersion: String?
+        let meta: GLTFVRM_GLTFVRMMeta
+        let humanoid: GLTFVRM_GLTFVRMHumanoid
+        let firstPerson: GLTFVRM_GLTFVRMFirstperson
+        let blendShapeMaster: GLTFVRM_GLTFVRMBlendShapeMaster
+        let secondaryAnimation: GLTFVRM_GLTFVRMSecondaryAnimation
+        let materialProperties: [GLTFVRM_GLTFVRMMaterialProperties]
     }
     
-    public struct GLTFVRM_GLTFVRMMeta: Codable {
-        public let title: String
-        public let version: String
-        public let author: String
-        public let contactInformation: String
-        public let reference: String
-        public let texture: Int
-        public let allowedUserName: String
-        public let violentUssageName: String
-        public let sexualUssageName: String
-        public let commercialUssageName: String
-        public let otherPermissionUrl: String
-        public let licenseName: String
-        public let otherLicenseUrl: String
+    struct GLTFVRM_GLTFVRMMeta: Codable {
+        let title: String
+        let version: String
+        let author: String
+        let contactInformation: String
+        let reference: String
+        let texture: Int
+        let allowedUserName: String
+        let violentUssageName: String
+        let sexualUssageName: String
+        let commercialUssageName: String
+        let otherPermissionUrl: String
+        let licenseName: String
+        let otherLicenseUrl: String
     }
     
-    public struct GLTFVRM_GLTFVRMHumanoid: Codable {
-        public let humanBones: [GLTFVRM_GLTFVRMHumanBone]
-        public let armStretch: Float
-        public let legStretch: Float
-        public let upperArmTwist: Float
-        public let lowerArmTwist: Float
-        public let upperLegTwist: Float
-        public let lowerLegTwist: Float
-        public let feetSpacing: Float
-        public let hasTranslationDoF: Bool
+    struct GLTFVRM_GLTFVRMHumanoid: Codable {
+        let humanBones: [GLTFVRM_GLTFVRMHumanBone]
+        let armStretch: Float
+        let legStretch: Float
+        let upperArmTwist: Float
+        let lowerArmTwist: Float
+        let upperLegTwist: Float
+        let lowerLegTwist: Float
+        let feetSpacing: Float
+        let hasTranslationDoF: Bool
     }
     
-    public struct GLTFVRM_GLTFVRMHumanBone: Codable {
-        public let bone: String
-        public let node: Int
-        public let useDefaultValues: Bool
+    struct GLTFVRM_GLTFVRMHumanBone: Codable {
+        let bone: String
+        let node: Int
+        let useDefaultValues: Bool
     }
     
-    public struct GLTFVRM_GLTFVRMFirstperson: Codable {
-        public let firstPersonBone: Int
-        public let firstPersonBoneOffset: GLTFVRM_GLTFVRMVec3
-        public let meshAnnotations: [GLTFVRM_GLTFVRMMeshAnnotation]
-        public let lookAtTypeName: String
-        public let lookAtHorizontalInner: GLTFVRM_GLTFVRMDegreeMap
-        public let lookAtHorizontalOuter: GLTFVRM_GLTFVRMDegreeMap
-        public let lookAtVerticalDown: GLTFVRM_GLTFVRMDegreeMap
-        public let lookAtVerticalUp: GLTFVRM_GLTFVRMDegreeMap
+    struct GLTFVRM_GLTFVRMFirstperson: Codable {
+        let firstPersonBone: Int
+        let firstPersonBoneOffset: GLTFVRM_GLTFVRMVec3
+        let meshAnnotations: [GLTFVRM_GLTFVRMMeshAnnotation]
+        let lookAtTypeName: String
+        let lookAtHorizontalInner: GLTFVRM_GLTFVRMDegreeMap
+        let lookAtHorizontalOuter: GLTFVRM_GLTFVRMDegreeMap
+        let lookAtVerticalDown: GLTFVRM_GLTFVRMDegreeMap
+        let lookAtVerticalUp: GLTFVRM_GLTFVRMDegreeMap
     }
     
-    public struct GLTFVRM_GLTFVRMMeshAnnotation: Codable {
-        public let mesh: Int
-        public let firstPersonFlag: String
+    struct GLTFVRM_GLTFVRMMeshAnnotation: Codable {
+        let mesh: Int
+        let firstPersonFlag: String
     }
     
-    public struct GLTFVRM_GLTFVRMDegreeMap: Codable {
-        public let curve: [Float]?
-        public let xRange: Float
-        public let yRange: Float
+    struct GLTFVRM_GLTFVRMDegreeMap: Codable {
+        let curve: [Float]?
+        let xRange: Float
+        let yRange: Float
     }
     
-    public struct GLTFVRM_GLTFVRMBlendShapeMaster: Codable {
-        public let blendShapeGroups: [GLTFVRM_GLTFVRMBlendShapeGroup]
+    struct GLTFVRM_GLTFVRMBlendShapeMaster: Codable {
+        let blendShapeGroups: [GLTFVRM_GLTFVRMBlendShapeGroup]
     }
     
-    public struct GLTFVRM_GLTFVRMBlendShapeGroup: Codable {
-        public let name: String
-        public let presetName: String
-        public let binds: [GLTFVRM_GLTFVRMBind]
-        public let materialValues: [GLTFVRM_GLTFVRMMaterialValue]
+    struct GLTFVRM_GLTFVRMBlendShapeGroup: Codable {
+        let name: String
+        let presetName: String
+        let binds: [GLTFVRM_GLTFVRMBind]
+        let materialValues: [GLTFVRM_GLTFVRMMaterialValue]
     }
     
-    public struct GLTFVRM_GLTFVRMBind: Codable {
-        public let mesh: Int
-        public let index: Int
-        public let weight: Float
+    struct GLTFVRM_GLTFVRMBind: Codable {
+        let mesh: Int
+        let index: Int
+        let weight: Float
     }
     
-    public struct GLTFVRM_GLTFVRMMaterialValue: Codable {
+    struct GLTFVRM_GLTFVRMMaterialValue: Codable {
         
     }
     
-    public struct GLTFVRM_GLTFVRMSecondaryAnimation: Codable {
-        public let boneGroups: [GLTFVRM_GLTFVRMBoneGroup]
-        public let colliderGroups: [GLTFVRM_GLTFVRMColliderGroup]
+    struct GLTFVRM_GLTFVRMSecondaryAnimation: Codable {
+        let boneGroups: [GLTFVRM_GLTFVRMBoneGroup]
+        let colliderGroups: [GLTFVRM_GLTFVRMColliderGroup]
     }
     
-    public struct GLTFVRM_GLTFVRMBoneGroup: Codable {
-        public let comment: String
-        public let stiffiness: Float
-        public let gravityPower: Float
-        public let gravityDir: GLTFVRM_GLTFVRMVec3
-        public let dragForce: Float
-        public let center: Float
-        public let hitRadius: Float
-        public let bones: [Int]
-        public let colliderGroups: [Int]
+    struct GLTFVRM_GLTFVRMBoneGroup: Codable {
+        let comment: String
+        let stiffiness: Float
+        let gravityPower: Float
+        let gravityDir: GLTFVRM_GLTFVRMVec3
+        let dragForce: Float
+        let center: Float
+        let hitRadius: Float
+        let bones: [Int]
+        let colliderGroups: [Int]
     }
     
-    public struct GLTFVRM_GLTFVRMColliderGroup: Codable {
-        public let node: Int
-        public let colliders: [GLTFVRM_GLTFVRMCollider]
+    struct GLTFVRM_GLTFVRMColliderGroup: Codable {
+        let node: Int
+        let colliders: [GLTFVRM_GLTFVRMCollider]
     }
     
-    public struct GLTFVRM_GLTFVRMCollider: Codable {
-        public let offset: GLTFVRM_GLTFVRMVec3
-        public let radius: Float
+    struct GLTFVRM_GLTFVRMCollider: Codable {
+        let offset: GLTFVRM_GLTFVRMVec3
+        let radius: Float
     }
     
-    public struct GLTFVRM_GLTFVRMMaterialProperties: Codable {
-        public let name: String
-        public let shader: String
-        public let renderQueue: Int
-        public let floatProperties: [String: Float]
-        public let keywordMap: [String: Bool]
-        public let tagMap: [String: String]
+    struct GLTFVRM_GLTFVRMMaterialProperties: Codable {
+        let name: String
+        let shader: String
+        let renderQueue: Int
+        let floatProperties: [String: Float]
+        let keywordMap: [String: Bool]
+        let tagMap: [String: String]
     }
     
-    public struct GLTFVRM_GLTFVRMVec3: Codable {
-        public let x: Float
-        public let y: Float
-        public let z: Float
+    struct GLTFVRM_GLTFVRMVec3: Codable {
+        let x: Float
+        let y: Float
+        let z: Float
     }
     
-    public let data: GLTFVRM_VRM?
+    let data: GLTFVRM_VRM?
     
     enum CodingKeys: String, CodingKey {
         case data = "VRM"
     }
     
-    public func didLoad(by object: Any, unarchiver: GLTFUnarchiver) {
+    func didLoad(by object: Any, unarchiver: GLTFUnarchiver) {
         guard let data = self.data else { return }
         guard let scene = object as? SCNScene else { return }
+
+        let meta = data.meta
+        let dict: [String:Any] = [
+            "title": meta.title,
+            "author": meta.author,
+            "contactInformation": meta.contactInformation,
+            "reference": meta.reference,
+            "texture": meta.texture,
+            "version": meta.version,
+            "allowedUserName": meta.allowedUserName,
+            "violentUssageName": meta.violentUssageName,
+            "sexualUssageName": meta.sexualUssageName,
+            "commercialUssageName": meta.commercialUssageName,
+            "otherPermissionUrl": meta.otherPermissionUrl,
+            "licenseName": meta.licenseName,
+            "otherLicenseUrl": meta.otherLicenseUrl
+            ]
+        scene.setValue(dict, forKey: "VRMMeta")
         
         // TODO: Implement
         data.materialProperties.forEach { material in
