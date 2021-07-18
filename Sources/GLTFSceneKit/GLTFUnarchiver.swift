5d2077bf2779bba4f6ed1f5ff45c0f9aea93071f
magicien <magicien.du.ballon@gmail.com>
2021-07-19 00:17:32 +0900

    Add Swift Package Manager support

--- Sources/GLTFSceneKit/GLTFUnarchiver.swift
+++ Sources/GLTFSceneKit/GLTFUnarchiver.swift
@@ -15,6 +15,12 @@ let glbMagic = 0x46546C67 // "glTF"
 let chunkTypeJSON = 0x4E4F534A // "JSON"
 let chunkTypeBIN = 0x004E4942 // "BIN"
 
+#if SWIFT_PACKAGE
+let bundle = Bundle.module
+#else
+let bundle = Bundle(for: GLTFUnarchiver.self)
+#endif
+
 public class GLTFUnarchiver {
     private var directoryPath: URL? = nil
     private var json: GLTFGlTF! = nil
@@ -1042,12 +1048,12 @@ public class GLTFUnarchiver {
         material.isDoubleSided = glMaterial.doubleSided
         
         material.shaderModifiers = [
-            .surface: try! String(contentsOf: URL(fileURLWithPath: Bundle(for: GLTFUnarchiver.self).path(forResource: "GLTFShaderModifierSurface", ofType: "shader")!), encoding: String.Encoding.utf8)
+            .surface: try! String(contentsOf: URL(fileURLWithPath: bundle.path(forResource: "GLTFShaderModifierSurface", ofType: "shader")!), encoding: String.Encoding.utf8)
         ]
         #if SEEMS_TO_HAVE_DOUBLESIDED_BUG
             if material.isDoubleSided {
                 material.shaderModifiers = [
-                    .surface: try! String(contentsOf: URL(fileURLWithPath: Bundle(for: GLTFUnarchiver.self).path(forResource: "GLTFShaderModifierSurface_doubleSidedWorkaround", ofType: "shader")!), encoding: String.Encoding.utf8)
+                    .surface: try! String(contentsOf: URL(fileURLWithPath: bundle.path(forResource: "GLTFShaderModifierSurface_doubleSidedWorkaround", ofType: "shader")!), encoding: String.Encoding.utf8)
                 ]
             }
         #endif
@@ -1058,9 +1064,9 @@ public class GLTFUnarchiver {
         case "BLEND":
             material.blendMode = .alpha
             material.writesToDepthBuffer = false
-            material.shaderModifiers![.surface] = try! String(contentsOf: URL(fileURLWithPath: Bundle(for: GLTFUnarchiver.self).path(forResource: "GLTFShaderModifierSurface_alphaModeBlend", ofType: "shader")!), encoding: String.Encoding.utf8)
+            material.shaderModifiers![.surface] = try! String(contentsOf: URL(fileURLWithPath: bundle.path(forResource: "GLTFShaderModifierSurface_alphaModeBlend", ofType: "shader")!), encoding: String.Encoding.utf8)
         case "MASK":
-            material.shaderModifiers![.fragment] = try! String(contentsOf: URL(fileURLWithPath: Bundle(for: GLTFUnarchiver.self).path(forResource: "GLTFShaderModifierFragment_alphaCutoff", ofType: "shader")!), encoding: String.Encoding.utf8)
+            material.shaderModifiers![.fragment] = try! String(contentsOf: URL(fileURLWithPath: bundle.path(forResource: "GLTFShaderModifierFragment_alphaCutoff", ofType: "shader")!), encoding: String.Encoding.utf8)
         default:
             throw GLTFUnarchiveError.NotSupported("loadMaterial: alphaMode \(glMaterial.alphaMode) is not supported")
         }
